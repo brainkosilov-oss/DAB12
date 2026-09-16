bea2c6eb4d899e5f236388db1f667b8de12a0429
brainkosilov-oss <brainkosilov@gmail.com>
2026-09-16 18:08:56 +0500

    Updated .gitignore

--- src/components/LeadForm/LeadForm.before-encoding-fix.tsx
+++ src/components/LeadForm/LeadForm.before-encoding-fix.tsx
@@ -1,4 +1,4 @@
-﻿import { useState, useRef, useCallback } from 'react'
+import { useState, useRef, useCallback } from 'react'
 import { ArrowUpRight, Upload, Close, Check } from '../Icons'
 
 interface LeadFormProps {
